43d5ed6ef2a93a04bae54c655481ae54cf106a78
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-21 18:53:52 +0100

    Add bevy_rapier2d to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .run();
 }
