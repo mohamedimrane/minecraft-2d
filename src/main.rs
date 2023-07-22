0477faffd6faaeea608f2d66a1d9c272007ccec3
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-22 18:45:15 +0100

    Add a todo list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,85 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
+#[derive(Component)]
+struct Speed(f32);
+
+#[derive(Component)]
+struct Jump(f32);
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugins((
+            DefaultPlugins,
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
+            RapierDebugRenderPlugin::default(),
+        ))
+        .insert_resource(RapierConfiguration {
+            gravity: Vec2::new(0., -1000.),
+            ..default()
+        })
+        .add_systems(Startup, (setup_graphics, setup_physics))
+        .add_systems(Update, (player_controller_movement))
         .run();
+}
+
+fn setup_graphics(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_physics(mut commands: Commands) {
+    spawn_player(&mut commands);
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::new(1000., 50.)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0., -300., 0.),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid(500., 25.),
+    ));
+}
+
+fn player_controller_movement(
+    mut player_query: Query<(&Speed, &mut Velocity)>,
+    keys: Res<Input<KeyCode>>,
+) {
+    for (speed, mut rb_vel) in player_query.iter_mut() {
+        // let up = keys.any_pressed([KeyCode::W, KeyCode::Up]);
+        // let down = keys.any_pressed([KeyCode::S, KeyCode::Down]);
+        let left = keys.any_pressed([KeyCode::A, KeyCode::Left]);
+        let right = keys.any_pressed([KeyCode::D, KeyCode::Right]);
+
+        let x_axis = -(left as i8) + right as i8;
+        // let y_axis = -(down as i8) + up as i8;
+
+        let move_delta_x = x_axis as f32;
+
+        rb_vel.linvel.x = move_delta_x * speed.0;
+    }
+}
+
+fn spawn_player(commands: &mut Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::splat(100.)),
+                ..default()
+            },
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Velocity::default(),
+        ExternalImpulse::default(),
+        Collider::cuboid(50., 50.),
+        LockedAxes::ROTATION_LOCKED,
+        Speed(300.),
+        Jump(100.),
+    ));
 }
