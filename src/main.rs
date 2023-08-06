ba5e2faa384151ccfb0517a78a1dd9c02a30ad4c
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-08-06 16:55:05 +0100

    Extract player logic into own plugin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use player::PlayerPlugin;
 
-#[derive(Component)]
-struct Speed(f32);
-
-#[derive(Component)]
-struct Jump(f32);
+mod player;
 
 fn main() {
     App::new()
@@ -13,13 +10,13 @@ fn main() {
             DefaultPlugins,
             RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
             RapierDebugRenderPlugin::default(),
+            PlayerPlugin,
         ))
         .insert_resource(RapierConfiguration {
             gravity: Vec2::new(0., -1000.),
             ..default()
         })
         .add_systems(Startup, (setup_graphics, setup_physics))
-        .add_systems(Update, (player_controller_movement))
         .run();
 }
 
@@ -28,8 +25,6 @@ fn setup_graphics(mut commands: Commands) {
 }
 
 fn setup_physics(mut commands: Commands) {
-    spawn_player(&mut commands);
-
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -42,44 +37,5 @@ fn setup_physics(mut commands: Commands) {
         },
         RigidBody::Fixed,
         Collider::cuboid(500., 25.),
-    ));
-}
-
-fn player_controller_movement(
-    mut player_query: Query<(&Speed, &mut Velocity)>,
-    keys: Res<Input<KeyCode>>,
-) {
-    for (speed, mut rb_vel) in player_query.iter_mut() {
-        // let up = keys.any_pressed([KeyCode::W, KeyCode::Up]);
-        // let down = keys.any_pressed([KeyCode::S, KeyCode::Down]);
-        let left = keys.any_pressed([KeyCode::A, KeyCode::Left]);
-        let right = keys.any_pressed([KeyCode::D, KeyCode::Right]);
-
-        let x_axis = -(left as i8) + right as i8;
-        // let y_axis = -(down as i8) + up as i8;
-
-        let move_delta_x = x_axis as f32;
-
-        rb_vel.linvel.x = move_delta_x * speed.0;
-    }
-}
-
-fn spawn_player(commands: &mut Commands) {
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::WHITE,
-                custom_size: Some(Vec2::splat(100.)),
-                ..default()
-            },
-            ..default()
-        },
-        RigidBody::Dynamic,
-        Velocity::default(),
-        ExternalImpulse::default(),
-        Collider::cuboid(50., 50.),
-        LockedAxes::ROTATION_LOCKED,
-        Speed(300.),
-        Jump(100.),
     ));
 }
---
+++ src/player.rs
@@ -0,0 +1,56 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, player_controller_movement);
+    }
+}
+
+#[derive(Component)]
+pub struct Speed(pub f32);
+
+#[derive(Component)]
+pub struct Jump(pub f32);
+
+pub fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(Vec2::splat(100.)),
+                ..default()
+            },
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Velocity::default(),
+        ExternalImpulse::default(),
+        Collider::cuboid(50., 50.),
+        LockedAxes::ROTATION_LOCKED,
+        Speed(300.),
+        Jump(100.),
+    ));
+}
+
+pub fn player_controller_movement(
+    mut player_query: Query<(&Speed, &mut Velocity)>,
+    keys: Res<Input<KeyCode>>,
+) {
+    for (speed, mut rb_vel) in player_query.iter_mut() {
+        // let up = keys.any_pressed([KeyCode::W, KeyCode::Up]);
+        // let down = keys.any_pressed([KeyCode::S, KeyCode::Down]);
+        let left = keys.any_pressed([KeyCode::A, KeyCode::Left]);
+        let right = keys.any_pressed([KeyCode::D, KeyCode::Right]);
+
+        let x_axis = -(left as i8) + right as i8;
+        // let y_axis = -(down as i8) + up as i8;
+
+        let move_delta_x = x_axis as f32;
+
+        rb_vel.linvel.x = move_delta_x * speed.0;
+    }
+}
