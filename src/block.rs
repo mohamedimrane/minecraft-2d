91f152d09767ea03f2503b083898596635dd42d3
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-08-11 10:52:21 +0100

    Handle texture loading for blocks

--- src/block.rs
+++ src/block.rs
@@ -1,10 +1,48 @@
-use bevy::{prelude::*, render::texture::DEFAULT_IMAGE_HANDLE};
-use bevy_rapier2d::prelude::*;
+use bevy::{math::vec2, prelude::*, render::texture::DEFAULT_IMAGE_HANDLE};
 
 // PLUGINS
 
 pub struct BlockPlugin;
 
 impl Plugin for BlockPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app
+            // Resources
+            .insert_resource(BlockGraphics::default())
+            // Systems
+            .add_systems(PreStartup, load_block_graphics)
+            // Reflection
+            ;
+    }
+}
+
+// RESOURCES
+
+#[derive(Resource)]
+struct BlockGraphics {
+    tex: Handle<Image>,
+    atlas_handle: Handle<TextureAtlas>,
+}
+
+impl Default for BlockGraphics {
+    fn default() -> Self {
+        Self {
+            tex: DEFAULT_IMAGE_HANDLE.typed(),
+            atlas_handle: Handle::<TextureAtlas>::default(),
+        }
+    }
+}
+
+// SYSTEMS
+
+fn load_block_graphics(
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut block_graphics: ResMut<BlockGraphics>,
+) {
+    block_graphics.tex = asset_server.load("blocks.png");
+    let atlas =
+        TextureAtlas::from_grid(block_graphics.tex.clone(), vec2(16., 16.), 4, 1, None, None);
+    let atlas_handle = texture_atlases.add(atlas);
+    block_graphics.atlas_handle = atlas_handle;
 }
