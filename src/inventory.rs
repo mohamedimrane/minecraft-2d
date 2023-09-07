2e6b3546e5f47c73636191b28d352e3e4c0c4137
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-09-07 17:55:01 +0100

    Add inspector for inventory

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::ResourceInspectorPlugin;
 
 use crate::item_kind::ItemKind;
 
@@ -14,14 +15,15 @@ pub struct InventoryPlugin;
 
 impl Plugin for InventoryPlugin {
     fn build(&self, app: &mut App) {
-        app 
+        app
             // Resources
             .insert_resource(CurrentItem::default())
             .insert_resource(Inv::default())
             // Systems
             .add_systems(Update, (manage_block_selection_inv, manage_hotbar_cursor))
             // Reflection
-        ;
+            .register_type::<Inv>()
+            .add_plugins(ResourceInspectorPlugin::<Inv>::default());
     }
 }
 
@@ -32,13 +34,13 @@ type Inv = Inventory<INVENTORY_SIZE, HOTBAR_SIZE, STACK_SIZE>;
 #[derive(Resource, Default)]
 pub struct CurrentItem(pub ItemKind);
 
-#[derive(Resource)]
-struct Inventory<const INVENTORY_SIZE: usize, const HOTBAR_SIZE: usize, const STACK_SIZE: usize> { 
-    items: [Option<InventorySlot>; INVENTORY_SIZE], 
-    hotbar_cursor: usize 
+#[derive(Resource, Reflect)]
+struct Inventory<const INVENTORY_SIZE: usize, const HOTBAR_SIZE: usize, const STACK_SIZE: usize> {
+    items: [Option<InventorySlot>; INVENTORY_SIZE],
+    hotbar_cursor: usize,
 }
 
-#[derive(Clone, Copy, Default)]
+#[derive(Clone, Copy, Default, Reflect)]
 struct InventorySlot {
     kind: ItemKind,
     quantity: usize,
@@ -75,51 +77,45 @@ impl<const I: usize, const H: usize, const S: usize> Inventory<I, H, S> {
 impl<const I: usize, const H: usize, const S: usize> Default for Inventory<I, H, S> {
     fn default() -> Self {
         Self {
-            items: [Some(default()); I], 
-            hotbar_cursor: 0
+            items: [Some(default()); I],
+            hotbar_cursor: 0,
         }
     }
 }
 
 // SYSTEMS
 
-fn manage_block_selection_inv(
-    mut current_item: ResMut<CurrentItem>,
-    keys: Res<Input<KeyCode>>
-) {
+fn manage_block_selection_inv(mut current_item: ResMut<CurrentItem>, keys: Res<Input<KeyCode>>) {
     for k in keys.get_pressed() {
         current_item.0 = match k {
-            KeyCode::Key1 => ItemKind::Dirt, 
-            KeyCode::Key2 => ItemKind::Grass, 
-            KeyCode::Key3 => ItemKind::Stone, 
-            KeyCode::Key4 => ItemKind::Cobblestone, 
-            KeyCode::Key5 => ItemKind::Deepslate, 
-            KeyCode::Key6 => ItemKind::CobbledDeepslate, 
-            KeyCode::Key7 => ItemKind::Bedrock, 
-            KeyCode::Key8 => ItemKind::HayBale, 
-            KeyCode::Key9 => ItemKind::OakLog, 
-            KeyCode::Key0 => ItemKind::OakPlank, 
-            _ => current_item.0
+            KeyCode::Key1 => ItemKind::Dirt,
+            KeyCode::Key2 => ItemKind::Grass,
+            KeyCode::Key3 => ItemKind::Stone,
+            KeyCode::Key4 => ItemKind::Cobblestone,
+            KeyCode::Key5 => ItemKind::Deepslate,
+            KeyCode::Key6 => ItemKind::CobbledDeepslate,
+            KeyCode::Key7 => ItemKind::Bedrock,
+            KeyCode::Key8 => ItemKind::HayBale,
+            KeyCode::Key9 => ItemKind::OakLog,
+            KeyCode::Key0 => ItemKind::OakPlank,
+            _ => current_item.0,
         }
     }
 }
 
-fn manage_hotbar_cursor(
-    mut inventory: ResMut<Inv>,
-    keys: Res<Input<KeyCode>>
-) {
+fn manage_hotbar_cursor(mut inventory: ResMut<Inv>, keys: Res<Input<KeyCode>>) {
     for k in keys.get_pressed() {
         inventory.hotbar_cursor = match k {
-            KeyCode::Key1 => 0, 
-            KeyCode::Key2 => 1, 
-            KeyCode::Key3 => 2, 
-            KeyCode::Key4 => 3, 
-            KeyCode::Key5 => 4, 
-            // KeyCode::Key6 => 5, 
-            // KeyCode::Key7 => 6, 
-            // KeyCode::Key8 => 7, 
-            // KeyCode::Key9 => 8, 
-            _ => inventory.hotbar_cursor
+            KeyCode::Key1 => 0,
+            KeyCode::Key2 => 1,
+            KeyCode::Key3 => 2,
+            KeyCode::Key4 => 3,
+            KeyCode::Key5 => 4,
+            // KeyCode::Key6 => 5,
+            // KeyCode::Key7 => 6,
+            // KeyCode::Key8 => 7,
+            // KeyCode::Key9 => 8,
+            _ => inventory.hotbar_cursor,
         };
 
         break;
