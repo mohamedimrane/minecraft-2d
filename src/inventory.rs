9e985af7de3405e59f2f76d12432c4e67a7bb52e
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-09-09 20:16:41 +0100

    Create unfunctional ui for inventory

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,13 +1,19 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::ResourceInspectorPlugin;
 
-use crate::item_kind::ItemKind;
+use crate::{block::BlockGraphics, item_kind::ItemKind};
 
 // CONSTANTS
 
-const INVENTORY_SIZE: usize = 10;
-const HOTBAR_SIZE: usize = 5;
-const STACK_SIZE: usize = 3;
+const INVENTORY_SIZE: usize = 36;
+const HOTBAR_SIZE: usize = 9;
+const STACK_SIZE: usize = 5;
+
+const UI_HOTBAR_BOTTOM_SPACING: f32 = 10.;
+const UI_ITEM_SIZE: f32 = 40.;
+const UI_SLOT_SIZE: f32 = UI_ITEM_SIZE * 1.4;
+const UI_SLOT_SPACING: f32 = 5.;
+const UI_SLOT_FONT_SIZE: f32 = 23.;
 
 // PLUGINS
 
@@ -19,6 +25,7 @@ impl Plugin for InventoryPlugin {
             // Resources
             .insert_resource(Inv::default())
             // Systems
+            .add_systems(Startup, spawn_ui)
             .add_systems(Update, manage_hotbar_cursor)
             // Reflection
             .register_type::<Inv>()
@@ -27,6 +34,119 @@ impl Plugin for InventoryPlugin {
 }
 
 // SYSTEMS
+
+fn spawn_ui(mut commands: Commands, block_graphics: Res<BlockGraphics>) {
+    commands
+        .spawn((NodeBundle {
+            // background_color: Color::GREEN.into(),
+            style: Style {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::End,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        },))
+        .with_children(|cb| spawn_hotbar(cb, &block_graphics));
+}
+
+fn spawn_hotbar(cb: &mut ChildBuilder, block_graphics: &Res<BlockGraphics>) {
+    cb.spawn((
+        HotbarUi,
+        NodeBundle {
+            // background_color: Color::RED.into(),
+            style: Style {
+                width: Val::Px(
+                    HOTBAR_SIZE as f32 * UI_SLOT_SIZE + (HOTBAR_SIZE as f32 - 1.) * UI_SLOT_SPACING,
+                ),
+                height: Val::Px(UI_SLOT_SIZE),
+                justify_content: JustifyContent::SpaceBetween,
+                margin: UiRect {
+                    bottom: Val::Px(UI_HOTBAR_BOTTOM_SPACING),
+                    ..default()
+                },
+                ..default()
+            },
+            ..default()
+        },
+    ))
+    .with_children(|cb| {
+        for i in 0..HOTBAR_SIZE {
+            spawn_slot(cb, block_graphics);
+        }
+    });
+}
+
+fn spawn_slot(cb: &mut ChildBuilder, block_graphics: &Res<BlockGraphics>) {
+    cb.spawn((
+        SlotUi,
+        NodeBundle {
+            // background_color: Color::BLUE.into(),
+            style: Style {
+                width: Val::Px(UI_SLOT_SIZE),
+                height: Val::Px(UI_SLOT_SIZE),
+                ..default()
+            },
+            ..default()
+        },
+    ))
+    .with_children(|cb| {
+        spawn_slot_image(cb, block_graphics);
+        spawn_slot_text(cb)
+    });
+}
+
+fn spawn_slot_image(cb: &mut ChildBuilder, block_graphics: &Res<BlockGraphics>) {
+    cb.spawn(AtlasImageBundle {
+        texture_atlas: block_graphics.atlas_handle.clone(),
+        texture_atlas_image: UiTextureAtlasImage {
+            index: 0,
+            ..default()
+        },
+        style: Style {
+            width: Val::Percent(90.),
+            height: Val::Percent(90.),
+            ..default()
+        },
+        ..default()
+    });
+}
+
+fn spawn_slot_text(cb: &mut ChildBuilder) {
+    cb.spawn(TextBundle {
+        text: Text {
+            sections: vec![TextSection::new(
+                "1",
+                TextStyle {
+                    font_size: UI_SLOT_FONT_SIZE,
+                    ..default()
+                },
+            )],
+            ..default()
+        },
+        style: Style {
+            position_type: PositionType::Absolute,
+            right: Val::Px(UI_SLOT_SPACING + 3.),
+            bottom: Val::Px(3.),
+            ..default()
+        },
+        ..default()
+    });
+}
+
+// fn toggle_ui(mut ui: Query<&mut Visibility, With<InventoryUi>>, keys: Res<Input<KeyCode>>) {
+//     if !keys.just_pressed(KeyCode::O) {
+//         return;
+//     }
+
+//     let mut ui = ui.single_mut();
+//     *ui = match *ui {
+//         Visibility::Inherited | Visibility::Visible => Visibility::Hidden,
+//         Visibility::Hidden => Visibility::Inherited,
+//     };
+// }
 
 fn manage_hotbar_cursor(mut inventory: ResMut<Inv>, keys: Res<Input<KeyCode>>) {
     for k in keys.get_pressed() {
@@ -146,3 +266,11 @@ impl<const I: usize, const H: usize, const S: usize> Default for Inventory<I, H,
         }
     }
 }
+
+// COMPONENTS
+
+#[derive(Component)]
+struct HotbarUi;
+
+#[derive(Component)]
+struct SlotUi;
